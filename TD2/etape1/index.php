832bd3576eacbe18969f0f2a273adc91edb4f4cc
Blue-Widge <59555837+Blue-Widge@users.noreply.github.com>
2020-05-25 01:56:53 +0200

    Etape 1 faite

--- TD2/etape1/index.php
+++ TD2/etape1/index.php
@@ -1,4 +1,3 @@
-<!-- Etape 1 : tester le formulaire avec php -->
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -8,24 +7,23 @@
 	<body>
         <form action="" method ="POST">
             <input type="text" name="nom" value="">
-            ______________________________________
-            ______________________________________
-            ______________________________________
-            ______________________________________
+            <input type="text" name="prenom" value="">
+            <input type="text" name="statut" value="">
+            <input type="text" name="date" value="">
+            <input type="submit"  value="Submit">
         </form>
         <?php
             // récupération des données du formulaire
             $nom= $_POST['nom'];	
-            $prenom= _____________
-            $statut= _____________
-            $date= _____________
+            $prenom= $_POST['prenom'];
+            $statut= $_POST['statut'];
+            $date= $_POST['date'];
             //afficher les données ici dans des div 
-            echo "<div>_____________<div>";
-            _____________
-            _____________
-            _____________
+            echo "<div> $nom </div>";
+			echo "<div> $prenom </div>";
+			echo "<div> $statut </div>";
+			echo "<div> $date </div>";
         ?>
     </body>
   
 </html>
-
